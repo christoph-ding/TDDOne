3b43fee2eda1ec419e9bc67c5ed9a60da2e73882
Pokegear <christoph.ding@gmail.com>
2016-04-21 22:45:37 -0400

    Started OOP for CartSummary.  Wrote some tests for CartSummary.

---
+++ tests/part1/cart-summary-test.js
@@ -0,0 +1,29 @@
+var chai = require('chai');
+var expect = chai.expect;
+var CartSummary = require('./../../src/part1/cart-summary.js');
+
+describe('CartSummary', function() {
+  it('getSubtotal() should return 0 if nothing was passed in', function() {
+    var cartSummary = new CartSummary([]);
+    expect(cartSummary.getSubtotal()).to.equal(0);
+  })
+
+  it('getSubtotal() return the total price of all the all items in the cart', function() {
+    var cartSummary = new CartSummary([{
+    id: 1,
+    quantity: 4,
+    price: 50
+    }, {
+    id: 2,
+    quantity: 2,
+    price: 30
+    }, {
+    id: 3,
+    quantity: 1,
+    price: 40
+    }]);
+    expect(cartSummary.getSubtotal()).to.equal(300);
+  })
+
+
+});
